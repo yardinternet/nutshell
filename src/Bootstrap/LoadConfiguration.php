21113d1ffec0d4cfcd80f628d908b61b79fa4c78
ictbeheer <ictbeheer@users.noreply.github.com>
2024-11-28 09:06:48 +0000

    (auto): apply php-cs-fixer changes

--- src/Bootstrap/LoadConfiguration.php
+++ src/Bootstrap/LoadConfiguration.php
@@ -34,8 +34,9 @@ class LoadConfiguration extends AcornLoadConfiguration
 
 		foreach ($files as $key => $path) {
 			$repository->set($key, array_merge(
-                require $path, $repository->get($key, [])
-            ));
+				require $path,
+				$repository->get($key, [])
+			));
 		}
 	}
 }
