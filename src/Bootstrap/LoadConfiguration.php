897c41d78cdd4b15867077169319174222f8fa9d
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2024-12-04 14:23:59 +0100

    (chore) fix PHPstan errors

--- src/Bootstrap/LoadConfiguration.php
+++ src/Bootstrap/LoadConfiguration.php
@@ -5,26 +5,27 @@ declare(strict_types=1);
 namespace Yard\BraveChild\Bootstrap;
 
 use Illuminate\Contracts\Config\Repository as RepositoryContract;
-use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Foundation\Application as ApplicationContract;
+use Illuminate\Foundation\Application;
 use Roots\Acorn\Bootstrap\LoadConfiguration as AcornLoadConfiguration;
 
 class LoadConfiguration extends AcornLoadConfiguration
 {
-	public function bootstrap(Application $app)
+	public function bootstrap(ApplicationContract $app)
 	{
 		parent::bootstrap($app);
 
 		if (! is_child_theme()) {
 			return;
 		}
-
+		/** @var Application*/
 		$childApp = clone $app;
 		$childApp->useConfigPath(get_stylesheet_directory() . '/config');
 
-		$this->loadChildConfigurationFiles($childApp, $app['config']);
+		$this->loadChildConfigurationFiles($childApp, $app->get('config'));
 	}
 
-	public function loadChildConfigurationFiles(Application $app, RepositoryContract $repository)
+	public function loadChildConfigurationFiles(Application $app, RepositoryContract $repository): void
 	{
 		$files = $this->getConfigurationFiles($app);
 
--- src/View/ViewServiceProvider.php
+++ src/View/ViewServiceProvider.php
@@ -5,16 +5,19 @@ declare(strict_types=1);
 namespace Yard\BraveChild\View;
 
 use Illuminate\Support\Arr;
+use Roots\Acorn\View\Composer;
 use Roots\Acorn\View\ViewServiceProvider as ViewViewServiceProvider;
 
 class ViewServiceProvider extends ViewViewServiceProvider
 {
 	public function attachComposers()
 	{
-		$composers = $this->app->config['view.composers'];
+		/** @var array<Composer>*/
+		$composers = $this->app->get('config')->get('view.composers', []);
 
 		if (is_array($composers) && Arr::isAssoc($composers)) {
 			foreach ($composers as $composer) {
+				// @phpstan-ignore method.notFound
 				$this->view()->composer($composer::views(), $composer);
 			}
 		}
