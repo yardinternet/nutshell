a9268da5a8e9a2fb01c0a1eeeb7d7998c96a0c1b
YvetteNikolov <yvettenikolov@gmail.com>
2025-04-11 17:20:46 +0200

    (feat): add vite alias from acorn 5.0

---
+++ src/Assets/Vite.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yard\SageChildThemeSupport\Assets;
+
+use Illuminate\Foundation\Vite as FoundationVite;
+
+use function Roots\asset;
+
+class Vite extends FoundationVite
+{
+	/**
+	 * Generate an asset path for the application.
+	 *
+	 * @param  string  $path
+	 * @param  bool|null  $secure
+	 *
+	 * @return string
+	 */
+	protected function assetPath($path, $secure = null)
+	{
+		return str_replace('/build/build/', '/build/', asset($path)->uri());
+	}
+}
