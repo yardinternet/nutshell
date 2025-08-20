a589c6a85f451843b8292bdc0fc4133647b86217
rivanuff <rivanuff@gmail.com>
2025-08-20 15:35:40 +0200

    (feat): register exceptions and middleware through laravel way

--- src/helpers.php
+++ src/helpers.php
@@ -5,12 +5,8 @@ declare(strict_types=1);
 namespace Yard\Nutshell;
 
 use Roots\Acorn\Application;
-use Roots\Acorn\Configuration\ApplicationBuilder;
-use Roots\Acorn\Configuration\Exceptions;
-use Roots\Acorn\Configuration\Middleware;
 use Sentry\Laravel\Integration;
-use Spatie\Csp\AddCspHeaders;
-use Yard\Logging\Log;
+use Throwable;
 
 function bootloader(?string $basePath = null): ApplicationBuilder
 {
@@ -18,15 +14,16 @@ function bootloader(?string $basePath = null): ApplicationBuilder
 		->withBindings([
 			\Roots\Acorn\Bootstrap\LoadConfiguration::class => \Yard\Nutshell\Bootstrap\LoadConfiguration::class,
 			\Roots\Acorn\Console\Kernel::class => \Yard\Nutshell\Console\Kernel::class,
+			\Yard\Nutshell\Assets\Vite::class => \Illuminate\Foundation\Vite::class,
 		])
-		->withExceptions(function (Exceptions $exceptions) {
-			$exceptions->report(function (\Throwable $e) {
+		->withExceptions(function ($exceptions) {
+			$exceptions->report(function (Throwable $e) {
 				Integration::captureUnhandledException($e);
 			});
-		})
-		->withMiddleware(function (Middleware $middleware) {
-			$middleware->append(AddCspHeaders::class);
-		})
+    	})
+		->withMiddleware([
+			\Spatie\Csp\AddCspHeaders::class
+		])
 		->withRouting(wordpress: true)
 		->withPaths(public: get_theme_file_path('public'));
 
