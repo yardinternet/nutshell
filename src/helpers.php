b7e80e9d14b1a267f1b820d6c595ddc39f55565c
rivanuff <rivanuff@gmail.com>
2025-08-20 15:13:04 +0200

    (feat): acorn 5.0 + php 8.2

--- src/helpers.php
+++ src/helpers.php
@@ -4,42 +4,33 @@ declare(strict_types=1);
 
 namespace Yard\Nutshell;
 
-use Roots\Acorn\Bootloader;
+use Roots\Acorn\Application;
+use Roots\Acorn\Configuration\ApplicationBuilder;
+use Roots\Acorn\Configuration\Exceptions;
+use Roots\Acorn\Configuration\Middleware;
+use Sentry\Laravel\Integration;
+use Spatie\Csp\AddCspHeaders;
 use Yard\Logging\Log;
 
-function bootloader(): Bootloader
+function bootloader(?string $basePath = null): ApplicationBuilder
 {
-	$bootloader = \Roots\bootloader();
+	$bootloader = Application::configure($basePath)
+		->withBindings([
+			\Roots\Acorn\Bootstrap\LoadConfiguration::class => \Yard\Nutshell\Bootstrap\LoadConfiguration::class,
+			\Roots\Acorn\Console\Kernel::class => \Yard\Nutshell\Console\Kernel::class,
+		])
+		->withExceptions(function (Exceptions $exceptions) {
+			$exceptions->report(function (\Throwable $e) {
+				Integration::captureUnhandledException($e);
+			});
+		})
+		->withMiddleware(function (Middleware $middleware) {
+			$middleware->append(AddCspHeaders::class);
+		})
+		->withRouting(wordpress: true)
+		->withPaths(public: get_theme_file_path('public'));
 
-	$bootloader->getApplication()->bind(
-		\Roots\Acorn\Bootstrap\LoadConfiguration::class,
-		\Yard\Nutshell\Bootstrap\LoadConfiguration::class
-	);
-
-	$bootloader->getApplication()->bind(
-		\Roots\Acorn\Console\Kernel::class,
-		\Yard\Nutshell\Console\Kernel::class
-	);
-
-	$bootloader->getApplication()->bind(
-		\Roots\Acorn\Http\Kernel::class,
-		\Yard\Nutshell\Http\Kernel::class
-	);
-
-	$bootloader->getApplication()->bind(
-		\Roots\Acorn\Exceptions\Handler::class,
-		\Yard\Nutshell\Exceptions\Handler::class
-	);
-
-	$bootloader->getApplication()->alias(
-		\Yard\Nutshell\Assets\Vite::class,
-		\Illuminate\Foundation\Vite::class
-	);
-
-	$bootloader->getApplication()->usePublicPath(get_theme_file_path('public'));
-
-	// Push Laravel logger to WordPress plugins
-	do_action(Log::WP_ACTION_SET_LOGGER, $bootloader->getApplication()->make('log'));
+	do_action(Log::WP_ACTION_SET_LOGGER, $bootloader->create()->make('log'));
 
 	return $bootloader;
 }
