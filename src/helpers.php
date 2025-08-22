06fd17cdf8474b330d91a20a0481bce8db747384
rivanuff <rivanuff@gmail.com>
2025-08-22 09:27:28 +0200

    (fix): proper appending of middleware

--- src/helpers.php
+++ src/helpers.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace Yard\Nutshell;
 
 use Roots\Acorn\Application;
+use Roots\Acorn\Configuration\Exceptions;
+use Roots\Acorn\Configuration\Middleware;
 use Sentry\Laravel\Integration;
+use Spatie\Csp\AddCspHeaders;
 use Throwable;
 
 function bootloader(): Application
@@ -15,15 +18,14 @@ function bootloader(): Application
 			\Roots\Acorn\Bootstrap\LoadConfiguration::class => \Yard\Nutshell\Bootstrap\LoadConfiguration::class,
 			\Roots\Acorn\Console\Kernel::class => \Yard\Nutshell\Console\Kernel::class,
 		])
-		->withExceptions(function ($exceptions) {
+		->withExceptions(function (Exceptions $exceptions) {
 			$exceptions->report(function (Throwable $e) {
 				Integration::captureUnhandledException($e);
 			});
 		})
-		->withMiddleware([
-			\Spatie\Csp\AddCspHeaders::class,
-		])
-		// ->withRouting(wordpress: true)
+		->withMiddleware(function (Middleware $middleware) {
+     		$middleware->append(AddCspHeaders::class);
+		})
 		->boot()
 		->usePublicPath(get_theme_file_path('public'));
 
